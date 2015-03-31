2d7739de5d459f1fb5542bd3022c3d80a8d32f13
xiaoerge <xiaoerge616@aol.com>
2015-03-30 21:49:23 -0400

    not yet full text, but works

---
+++ src/main/java/com/xiaoerge/fulltextrss/RSSFeed.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xiaoerge.fulltextrss;
+
+/**
+ *
+ * @author xiaoerge
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Stores an RSS feed
+ */
+public class RSSFeed {
+
+    private String title;
+    private String link;
+    private String description;
+    private String language;
+    private String copyright;
+    private String pubDate;
+    private List<RSSFeedMessage> entries = new ArrayList<RSSFeedMessage>();
+
+    public RSSFeed(String title, String link, String description, String language,
+            String copyright, String pubDate) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.language = language;
+        this.copyright = copyright;
+        this.pubDate = pubDate;
+    }
+
+    public List<RSSFeedMessage> getMessages() {
+        return entries;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Feed [copyright=" + copyright + ", description=" + description
+                + ", language=" + language + ", link=" + link + ", pubDate="
+                + pubDate + ", title=" + title + "]";
+    }
+
+}
---
+++ src/main/java/com/xiaoerge/fulltextrss/RSSFeedProgram.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xiaoerge.fulltextrss;
+
+/**
+ *
+ * @author xiaoerge
+ */
+public class RSSFeedProgram {
+
+    public static void main(String[] args) {
+        RSSFeedParser parser = new RSSFeedParser("http://rss.cnn.com/rss/cnn_topstories.rss");
+        RSSFeed feed = parser.readFeed();
+        System.out.println(feed);
+        for (RSSFeedMessage message : feed.getMessages()) {
+            System.out.println(message);
+        }
+    }
+}
